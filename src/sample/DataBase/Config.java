59e0f4ac959a886c3f968f81a40880db3df3c25f
kryschtop <kryschtopenok@ukr.net>
2020-11-02 19:57:11 +0200

    Initial commit

---
+++ src/sample/DataBase/Config.java
@@ -0,0 +1,17 @@
+package sample.DataBase;
+
+
+
+
+    public class Config {
+        protected String TABLE_NAME="users";
+        protected String HOST="jdbc:mysql://server222.hosting.reg.ru/u0613437_easyit?useUnicode=true&serverTimezone=UTC";
+        protected String USER="u0613437_easyit";
+        protected String PASS="Test1234";
+
+
+    }
+
+
+
+
